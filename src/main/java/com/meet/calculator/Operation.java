0aa02102b1cd49045285c7ace266255be2bff5ad
Meet Borisagar <meetborisagar22@gmail.com>
2026-08-12 23:33:30 +0530

    refactor : Add Operation class with constructor and getters

---
+++ src/main/java/com/meet/calculator/Operation.java
@@ -0,0 +1,26 @@
+package com.meet.calculator;
+
+public class Operation {
+
+    private double firstNum;
+    private String operator;
+    private double secondNum;
+
+    public Operation(double firstNum, String operator, double secondNum) {
+        this.firstNum = firstNum;
+        this.operator = operator;
+        this.secondNum = secondNum;
+    }
+
+    public double getA() {
+        return firstNum;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public double getB() {
+        return secondNum;
+    }
+}
