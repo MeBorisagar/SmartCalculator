1973a9f546f002b964cc8330b52ccd54fc93710f
Meet Borisagar <meetborisagar22@gmail.com>
2026-08-23 11:14:09 +0530

    feat : Add SQRT and PCT operation

---
+++ src/main/java/com/meet/calculator/Percentage.java
@@ -0,0 +1,37 @@
+package com.meet.calculator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Percentage extends Operation implements Calculable{
+
+    private static final Logger log = LoggerFactory.getLogger(Percentage.class);
+
+    public Percentage(double firstNum) {
+        super(firstNum);
+    }
+
+    /**
+     * Percentage the first operand .
+     *
+     * @return the Percentage rounded to two decimal places.
+     */
+    @Override
+    public double calculate() {
+        double firstNumber = getFirstNum();
+
+        return firstNumber / 100;
+    }
+
+    /**
+     * Returns a readable representation of the percentage.
+     *
+     * @return the percentage expression and its result
+     */
+    @Override
+    public String toString() {
+        double firstNumber = getFirstNum();
+        double result = calculate();
+        return "Percentage: " + firstNumber + " % " + " = " + result;
+    }
+}
