fb431d2cd1eddfdb21d7881c8e06515eadf4da35
Meet Borisagar <meetborisagar22@gmail.com>
2026-08-27 21:21:45 +0530

    fix : Percentage Operation

--- src/main/java/com/meet/calculator/operations/Percentage.java
+++ src/main/java/com/meet/calculator/operations/Percentage.java
@@ -3,12 +3,10 @@ package com.meet.calculator.operations;
 import com.meet.calculator.Calculable;
 import com.meet.calculator.Operation;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 public class Percentage extends Operation implements Calculable {
 
-    private static final Logger log = LoggerFactory.getLogger(Percentage.class);
 
     public Percentage(double firstNum, double secondNum) {
         super(firstNum, secondNum);
@@ -24,13 +22,8 @@ public class Percentage extends Operation implements Calculable {
         double firstNumber = getFirstNum();
         double secondNumber = getSecondNum();
 
-        if (secondNumber == 0) {
-            log.warn("Error : Percentage by zero");
-            return Double.NaN;
-        }
 
-
-        double result = (firstNumber/secondNumber)*100;
+        double result = (firstNumber*secondNumber)/100;
 
         return Math.round(result * 100.0) / 100.0;
     }
@@ -45,9 +38,6 @@ public class Percentage extends Operation implements Calculable {
         double firstNumber = getFirstNum();
         double secondNumber = getSecondNum();
         double result = calculate();
-        if (Double.isNaN(result)) {
-            return "Percentage with zero " + firstNumber + " pct " + secondNumber + " not possible";
-        }
-        return "Percentage: " + firstNumber + " % " + secondNumber + " = " + result;
+        return "Percentage: " + secondNumber + " % of" + firstNumber + " = " + result;
     }
 }
